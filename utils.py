0f71342912e66b4167ddd0daec2ddd3bc8a05856
Derek Yu <derekyu@gmail.com>
2026-03-15 03:30:28 +0000

    Replace utils.py

--- utils.py
+++ utils.py
@@ -55,7 +55,7 @@ def classify_location(location_name):
         "united kingdom", "south korea", "united arab emirates", "uae", "switzerland",
         "poland", "south africa", "mexico", "portugal", "chile", "columbia", "colombia",
         "LATAM", "amsterdam", "buenos aires", "santiago", "london", "stockholm", "belgium",
-        "denmark", "lithuania"
+        "denmark", "lithuania", "peru", "ecuador"
     ]
 
     if is_japan:
